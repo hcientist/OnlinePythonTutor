06a23da79a5227c1aeb7ae6a6818a3e3b95fc0cc
Philip Guo <pg@cs.stanford.edu>
2011-09-23 15:46:03 -0700

    OMG WTF Internet Explorer!!!

--- edu-python.js
+++ edu-python.js
@@ -62,6 +62,19 @@ function assert(cond) {
   }
 }
 
+// OMG WTF Internet Explorer, I HATE YOU!!!
+// Thanks to: http://soledadpenades.com/2007/05/17/arrayindexof-in-internet-explorer/
+if (!Array.indexOf) {
+  Array.prototype.indexOf = function(obj){
+    for(var i=0; i<this.length; i++){
+        if(this[i]==obj){
+          return i;
+        }
+    }
+    return -1;
+  }
+}
+
 // taken from http://www.toao.net/32-my-htmlspecialchars-function-for-javascript
 function htmlspecialchars(str) {
   if (typeof(str) == "string") {
