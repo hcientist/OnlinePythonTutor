631c58f31e860e9d9e1d07331144e510186bbc58
Philip Guo <pg@cs.stanford.edu>
2011-09-28 00:12:20 -0400

    started trying to parse simple questions file format

---
+++ cgi-bin/parse_questions.py
@@ -0,0 +1,62 @@
+# Online Python Tutor
+# Copyright (C) 2010 Philip J. Guo
+# https://github.com/pgbovine/OnlinePythonTutor/
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Defines a function that parses an Online Python Tutor 'questions file'
+# into a dict, which can easily be converted into JSON
+
+import os, sys
+
+delimiters = set(['Name:', 'Question:', 'Hint:', 'Solution:',
+'Skeleton:', 'Test:', 'Expect:'])
+
+def parseQuestionsFile(filename):
+  ret = {}
+
+  curParts = []
+  curDelimiter = None
+
+  for line in open(filename):
+    # only strip TRAILING spaces and not leading spaces
+    line = line.rstrip()
+
+    if line in delimiters:
+      if curDelimiter == 'Name:':
+        ret['name'] = '\n'.join(curParts).strip()
+      elif curDelimiter == 'Question:':
+        ret['question'] = ' '.join(curParts).strip()
+      elif curDelimiter == 'Hint:':
+        ret['hint'] = ' '.join(curParts).strip()
+      elif curDelimiter == 'Solution:':
+        ret['solution'] = ' '.join(curParts).strip()
+      elif curDelimiter == 'Skeleton:':
+        ret['skeleton'] = '\n'.join(curParts).strip()
+
+      curDelimiter = line
+      curParts = []
+    else:
+      curParts.append(line)
+
+  if curDelimiter:
+    ret[curDelimiter] = '\n'.join(curParts)
+
+  return ret
+
+
+if __name__ == '__main__':
+  import pprint
+  pp = pprint.PrettyPrinter(indent=2)
+  print pp.pprint(parseQuestionsFile(sys.argv[1]))
