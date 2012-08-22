bc123867e30224431f2412e4de60ceca07c7a955
Philip Guo <pgbovine@google.com>
2012-08-22 12:21:02 -0700

    add http referer too

--- PyTutorGAE/logger/gae_logger.py
+++ PyTutorGAE/logger/gae_logger.py
@@ -8,11 +8,13 @@ class VisualizerRequest(db.Model):
   user_script = db.TextProperty(required=True) # LONG string
   cumulative_mode = db.BooleanProperty(requred=True)
   user_ip_addr = db.StringProperty(required=True)
+  http_referer = db.StringProperty(required=True)
   request_timestamp = db.DateTimeProperty(auto_now_add=True) # always set timestamp to NOW!
 
 x = VisualizerRequest(user_script=db.Text(u'print "hello world"\n', encoding='utf_8'),
                       cumulative_mode=True
-                      user_ip_addr='18.239.4.100')
+                      user_ip_addr='18.239.4.100',
+                      http_referer='http://www.google.com/?magic-search')
 
 x.put()
 
