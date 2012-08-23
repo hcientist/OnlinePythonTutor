91d41453fd3c2cc41dadcb8c74b52524d9f8cd51
Philip Guo <philip@pgbovine.net>
2012-08-23 00:13:58 -0700

    more

--- PyTutorGAE/logger/gae_logger.py
+++ PyTutorGAE/logger/gae_logger.py
@@ -9,12 +9,14 @@ class VisualizerRequest(db.Model):
   cumulative_mode = db.BooleanProperty(requred=True)
   user_ip_addr = db.StringProperty(required=True)
   http_referer = db.StringProperty(required=True)
+  app_id = db.StringProperty(required=True)
   request_timestamp = db.DateTimeProperty(auto_now_add=True) # always set timestamp to NOW!
 
 x = VisualizerRequest(user_script=db.Text(u'print "hello world"\n', encoding='utf_8'),
                       cumulative_mode=True
                       user_ip_addr='18.239.4.100',
-                      http_referer='http://www.google.com/?magic-search')
+                      http_referer='http://www.google.com/?magic-search',
+                      app_id='pgbovine test')
 
 x.put()
 
