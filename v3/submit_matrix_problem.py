5eae280c19996696fba23828235f1eb2a0815124
Philip Guo <philip@pgbovine.net>
2013-12-29 17:22:15 -0500

    update

--- v3/submit_matrix_problem.py
+++ v3/submit_matrix_problem.py
@@ -9,26 +9,10 @@ import urllib2
 form = cgi.FieldStorage()
 user_code = form['submitted_code'].value
 prob_name = form['problem_name'].value
-#assert type(prob_name) is str
-
-# whitelist
-assert prob_name in ('python_comprehension-1',)
-
-test_fn = 'matrix-demo/' + prob_name + '.test.py'
-test_cod = open(test_fn).read()
-
-# concatenate!
-script = test_cod + '\n' + user_code + \
-'''
-import doctest
-(n_fail, n_tests) = doctest.testmod(verbose=False)
-if n_fail == 0:
-  print("All %d tests passed!" % n_tests)
-'''
 
 # run on the EC2 sandbox
-url = 'http://ec2-107-20-94-197.compute-1.amazonaws.com/cgi-bin/run_code.py'
-values = {'user_script' : script}
+url = 'http://ec2-107-20-94-197.compute-1.amazonaws.com/cgi-bin/test_matrix_code.py'
+values = {'user_script' : script, 'problem_name': prob_name}
 
 data = urllib.urlencode(values)
 req = urllib2.Request(url, data)
