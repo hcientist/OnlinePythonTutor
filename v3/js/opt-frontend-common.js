86850f54a7de3843a8786753f71435ad0dfddc4d
Philip Guo <philip@pgbovine.net>
2014-07-04 16:16:00 -0400

    bah

--- v3/js/opt-frontend-common.js
+++ v3/js/opt-frontend-common.js
@@ -303,8 +303,8 @@ function initTogetherJS() {
     }
 
     $("#codeInputWarnings").html('<span style="color: #e93f34; font-weight: bold">\
-                                  Someone is typing ...</span> + \
-                                  $("#codeInputWarnings").data('orig-html')');
+                                  Someone is typing ...</span>' +
+                                  $("#codeInputWarnings").data('orig-html'));
 
     $.doTimeout('codeMirrorWarningTimeout', 1000, function() { // debounce
       $("#codeInputWarnings").html($("#codeInputWarnings").data('orig-html'));
