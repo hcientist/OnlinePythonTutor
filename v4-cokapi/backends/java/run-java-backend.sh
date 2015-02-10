2617ddb3007770b71a19f0a4dfd03d9ad048762e
Philip Guo <philip@pgbovine.net>
2015-02-10 03:30:07 +0000

    heredoc!

--- v4-cokapi/backends/java/run-java-backend.sh
+++ v4-cokapi/backends/java/run-java-backend.sh
@@ -4,6 +4,10 @@
 # expects as input, such as: java_jail/cp/traceprinter/test-input.txt
 
 # this will be run within Docker
-echo $1 | java -cp /tmp/java_jail/cp:/tmp/java_jail/cp/javax.json-1.0.jar:/usr/lib/jvm/java-7-openjdk-amd64/lib/tools.jar traceprinter.InMemory
 
-#echo '{ "usercode": "public class Test { public static void main(String[] args) { int x = 3; x += x; } }", "options": {}, "args": [], "stdin": "" }' | java -cp /tmp/java_jail/cp:/tmp/java_jail/cp/javax.json-1.0.jar:/usr/lib/jvm/java-7-openjdk-amd64/lib/tools.jar traceprinter.InMemory
+# tricky! use a heredoc to pipe the $1 argument into the stdin of the
+# java executable WITHOUT interpreting escape chars such as '\n' ...
+# echo doesn't work here since it interprets '\n' and other chars
+cat <<ENDEND | java -cp /tmp/java_jail/cp:/tmp/java_jail/cp/javax.json-1.0.jar:/usr/lib/jvm/java-7-openjdk-amd64/lib/tools.jar traceprinter.InMemory
+$1
+ENDEND
