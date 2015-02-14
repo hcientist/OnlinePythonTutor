f01d654a29daecbdde28a37514a6e9d57c8b2e0a
Philip Guo <philip@pgbovine.net>
2015-02-14 20:58:11 +0000

    changed java vm from 256MB to 128MB heap

--- v4-cokapi/backends/java/java_jail/cp/traceprinter/InMemory.java
+++ v4-cokapi/backends/java/java_jail/cp/traceprinter/InMemory.java
@@ -181,7 +181,7 @@ http://docs.oracle.com/javase/7/docs/jdk/api/jpda/jdi/com/sun/jdi/connect/Connec
 
             // set a memory limit
             
-            options += "-Xmx256M" + " "; // modified by pgbovine
+            options += "-Xmx128M" + " ";
             
             options += "-Dfile.encoding=UTF-8" + " ";
 
