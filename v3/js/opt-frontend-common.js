57390a6af9f0dd15d0af1465129df04fbeb7bc0b
Philip Guo <philip@pgbovine.net>
2015-08-16 11:02:56 -0700

    omg safari

--- v3/js/opt-frontend-common.js
+++ v3/js/opt-frontend-common.js
@@ -127,6 +127,25 @@ function generateUUID(){
 };
 
 
+// OMG nasty wtf?!?
+
+// From: http://stackoverflow.com/questions/21159301/quotaexceedederror-dom-exception-22-an-attempt-was-made-to-add-something-to-st
+
+// Safari, in Private Browsing Mode, looks like it supports localStorage but all calls to setItem
+// throw QuotaExceededError. We're going to detect this and just silently drop any calls to setItem
+// to avoid the entire page breaking, without having to do a check at each usage of Storage.
+if (typeof localStorage === 'object') {
+    try {
+        localStorage.setItem('localStorage', 1);
+        localStorage.removeItem('localStorage');
+    } catch (e) {
+        Storage.prototype._setItem = Storage.prototype.setItem;
+        Storage.prototype.setItem = function() {};
+        alert('Your web browser does not support storing settings locally. In Safari, the most common cause of this is using "Private Browsing Mode". Some settings may not save or some features may not work properly for you.');
+    }
+}
+
+
 var dmp = new diff_match_patch();
 var curCode = '';
 var deltaObj = undefined;
