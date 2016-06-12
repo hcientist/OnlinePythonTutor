ae73f0ab38c77b294300a07afde818b43512c8dd
Philip Guo <philip@pgbovine.net>
2016-06-12 14:11:53 -0400

    blah

--- v5-unity/README.txt
+++ v5-unity/README.txt
@@ -4,10 +4,15 @@ significantly clean up, modernize, and modularize the OPT codebase to
 ease future development.
 
 I started porting v3/ over to v5-unity/ on 2016-06-12 since the headache
-of manually maintaining so many JS/CSS files and intricate dependencies
-was starting to get out of hand ... i've waited for years to port to a
-more sustainable and modern development setup ...
+of manually maintaining so many JS/CSS files and their intricate
+dependencies was starting to get out of hand ... i've waited for years
+to port to a more sustainable and modern development setup ...
 
+I decided to go with Webpack for the module system and to upgrade the
+appropriate versions of libraries to match, without breaking crufty
+legacy code (hopefully)
+
+---
 
 Requires these global installations:
 - Node.js / npm
--- v5-unity/webpack.config.js
+++ v5-unity/webpack.config.js
@@ -1,12 +1,29 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: "./js/opt-frontend.js",
+
+    plugins: [
+      // http://stackoverflow.com/questions/29080148/expose-jquery-to-real-window-object-with-webpack
+      new webpack.ProvidePlugin({
+        // Automtically detect jQuery and $ as free var in modules
+        // and inject the jquery library
+        // This is required by many jquery plugins
+        jquery: "jquery",
+        jQuery: "jquery",
+        $: "jquery"
+      })
+    ],
 
     // some included libraries reference 'jquery', so point to it:
     resolve : {
         alias: {
-            "jquery": __dirname + "/js/jquery-1.8.2.min.js",
-            "$": __dirname + "/js/jquery-1.8.2.min.js",
-            "$.bbq": __dirname + "/js/jquery.ba-bbq.min.js",
+            "jquery": __dirname + "/js/jquery-3.0.0.min.js",
+            "$": __dirname + "/js/jquery-3.0.0.min.js",
+            //"jquery": __dirname + "/js/jquery-1.8.2.min.js",
+            //"$": __dirname + "/js/jquery-1.8.2.min.js",
+
+            "$.bbq": __dirname + "/js/jquery.ba-bbq.js",
         }
     },
 
