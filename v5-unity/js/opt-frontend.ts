82d4bbc7ba2afe3f6606678639ae00a8eb056d98
Philip Guo <philip@pgbovine.net>
2016-07-13 14:13:40 -0700

    bam

--- v5-unity/js/opt-frontend.ts
+++ v5-unity/js/opt-frontend.ts
@@ -4,8 +4,6 @@
 
 /* TODO:
 
-- make sure the "create test cases" feature still works
-
 - parse Java viz_options in users' java code:
   https://github.com/daveagp/java_visualize/blob/1489078712310eda44391f09405e0f71b2b190c9/jv-frontend.js#L101
 
@@ -53,6 +51,7 @@ require('script!./lib/socket.io-client/socket.io.js');
 
 // need to directly import the class for type checking to work
 import {AbstractBaseFrontend, generateUUID, supports_html5_storage} from './opt-frontend-common.ts';
+import {OptTestcases, redSadFace, yellowHappyFace} from './opt-testcases.ts';
 
 var pytutor = require('./pytutor.ts');
 var assert = pytutor.assert;
@@ -101,7 +100,7 @@ function optFrontendTogetherjsCloseHandler() {
 }
 */
 
-class OptFrontend extends AbstractBaseFrontend {
+export class OptFrontend extends AbstractBaseFrontend {
   originFrontendJsFile: string = 'opt-frontend.js';
   pyInputAceEditor; // Ace editor object that contains the user's code
 
@@ -791,7 +790,7 @@ class OptFrontend extends AbstractBaseFrontend {
 
 
 // OptFrontend augmented with a "Create test cases" pane
-class OptFrontendWithTestcases extends OptFrontend {
+export class OptFrontendWithTestcases extends OptFrontend {
   optTests: OptTestcases;
 
   constructor(params) {
@@ -904,217 +903,6 @@ class OptFrontendWithTestcases extends OptFrontend {
   }
 
 } // END Class OptFrontendWithTestcases
-
-
-
-// TODO: maybe extract to a new file called opt-testcases.ts ?
-
-const redSadFace = require('./images/red-sad-face.jpg');
-const yellowHappyFace = require('./images/yellow-happy-face.jpg');
-
-const testcasesPaneHtml = '\
-<table id="testCasesTable">\
-  <thead>\
-  <tr>\
-    <td style="width: 310px">Tests</td>\
-    <td><button id="runAllTestsButton" type="button">Run All Tests</button></td>\
-    <td>Results</td>\
-    <td></td>\
-    <td></td>\
-  </tr>\
-  </thead>\
-  <tbody>\
-  </tbody>\
-</table>\
-\
-<a href="#" id="addNewTestCase">Add new test</a>\
-'
-
-class OptTestcases {
-  parent: OptFrontendWithTestcases;
-  curTestcaseId: number = 1;
-
-  constructor(parent) {
-    this.parent = parent;
-    $("#testCasesParent")
-      .empty() // just to be paranoid, empty this out (and its event handlers, too, supposedly)
-      .html('<p style="margin-top: 25px;"><a href="#" id="createTestsLink">Create test cases</a></p><div id="testCasesPane"></div>');
-
-    $("#testCasesParent #createTestsLink").click(() => {
-      this.initTestcasesPane();
-      $("#testCasesParent #createTestsLink").hide();
-      return false;
-    });
-  }
-
-  initTestcasesPane() {
-    var _me = this;
-    $("#testCasesParent #testCasesPane")
-      .empty() // just to be paranoid, empty this out (and its event handlers, too, supposedly)
-      .html(testcasesPaneHtml);
-
-    $("#addNewTestCase").click(function() {
-      _me.addTestcase(null);
-      return false; // to prevent link from being followed
-    });
-
-    $("#runAllTestsButton").click(function() {
-      $(".runTestCase").click();
-    });
-  }
-
-  loadTestCases(lst: string[]) {
-    this.initTestcasesPane();
-    $("#testCasesParent #createTestsLink").hide();
-    lst.forEach((e) => {
-      this.addTestcase(e);
-    });
-  }
-
-  addTestcase(initialCod /* optional code to pre-seed this test */) {
-    var _me = this;
-
-    var id = this.curTestcaseId;
-    this.curTestcaseId++;
-    var newTr = $('<tr/>').attr('id', 'testCaseRow_' + id);
-    $("#testCasesParent #testCasesTable tbody").append(newTr);
-    var editorTd = $('<td/>');
-    var runBtnTd = $('<td/>');
-    var outputTd = $('<td/>');
-    var visualizeTd = $('<td/>');
-    var deleteTd = $('<td/>');
-
-    editorTd.append('<div id="testCaseEditor_' + id + '" class="testCaseEditor">');
-    runBtnTd.append('<button id="runTestCase_' + id + '" class="runTestCase" type="button">Run</button>');
-    outputTd.attr('id', 'outputTd_' + id);
-    outputTd.attr('class', 'outputTd');
-    visualizeTd.append('<button id="vizTestCase_' + id + '" class="vizTestCase" type="button">Visualize</button>');
-    deleteTd.append('<a id="delTestCase_' + id + '" href="javascript:void(0);">Delete test</a></td>');
-
-    newTr.append(editorTd);
-    newTr.append(runBtnTd);
-    newTr.append(outputTd);
-    newTr.append(visualizeTd);
-    newTr.append(deleteTd);
-
-    // initialize testCaseEditor with Ace:
-    var te = ace.edit('testCaseEditor_' + id);
-    // set the size and value ASAP to get alignment working well ...
-    te.setOptions({minLines: 2, maxLines: 4}); // keep this SMALL
-    te.setHighlightActiveLine(false);
-    te.setShowPrintMargin(false);
-    te.setBehavioursEnabled(false);
-    te.setFontSize('11px');
-    te.$blockScrolling = Infinity; // kludgy to shut up weird warnings
-
-    var s = te.getSession();
-    s.setTabSize(2);
-    s.setUseSoftTabs(true);
-    // disable extraneous indicators:
-    s.setFoldStyle('manual'); // no code folding indicators
-    // don't do real-time syntax checks:
-    // https://github.com/ajaxorg/ace/wiki/Syntax-validation
-    s.setOption("useWorker", false);
-    s.on("change", (e) => {
-      $('#outputTd_' + id).empty(); // remove all test output indicators
-    });
-
-    // TODO: change syntax highlighting mode if the user changes languages:
-    var lang = $('#pythonVersionSelector').val();
-    var mod = 'python';
-    var defaultVal = '\n# assert <test condition>';
-    if (lang === 'java') {
-      mod = 'java';
-      defaultVal = '// sorry, Java tests not yet supported';
-    } else if (lang === 'c' || lang === 'cpp') {
-      mod = 'c_cpp';
-      defaultVal = '// sorry, C/C++ tests not yet supported';
-    } else if (lang === 'js') {
-      mod = 'javascript';
-      defaultVal = '\n// console.assert(<test condition>);';
-    } else if (lang === 'ts') {
-      mod = 'typescript';
-      defaultVal = '\n// console.assert(<test condition>);';
-    } else if (lang === 'ruby') {
-      mod = 'ruby';
-      defaultVal = "\n# raise 'fail' unless <test condition>";
-    }
-    s.setMode("ace/mode/" + mod);
-
-    te.setValue(initialCod ? initialCod.rtrim() : defaultVal,
-                -1 /* do NOT select after setting text */);
-    te.focus();
-
-    function runOrVizTestCase(isViz /* true for visualize, false for run */) {
-      $("#runAllTestsButton,.runTestCase,.vizTestCase").attr('disabled', true);
-      var e = ace.edit('testCaseEditor_' + id);
-      e.getSession().clearAnnotations();
-      $('#outputTd_' + id).html('');
-
-      var dat = _me.getCombinedCode(id);
-      var cod = dat.cod;
-
-      if (isViz) {
-        $('#vizTestCase_' + id).html("Visualizing ...");
-        _me.parent.vizTestCase(id, cod, dat.firstTestLine);
-      } else {
-        $('#runTestCase_' + id).html("Running ...");
-        _me.parent.runTestCase(id, cod, dat.firstTestLine);
-      }
-    }
-
-    $('#runTestCase_' + id).click(runOrVizTestCase.bind(this, false));
-    $('#vizTestCase_' + id).click(runOrVizTestCase.bind(this, true));
-
-    $('#delTestCase_' + id).click(function() {
-      $('#testCaseRow_' + id).remove();
-      return false; // to prevent link from being followed
-    });
-  }
-
-  doneRunningTest() {
-    $("#runAllTestsButton,.runTestCase,.vizTestCase").attr('disabled', false);
-    $(".runTestCase").html('Run');
-    $(".vizTestCase").html('Visualize');
-  }
-
-  getCombinedCode(id) {
-    var userCod = this.parent.pyInputGetValue();
-    var testCod = ace.edit('testCaseEditor_' + id).getValue();
-    // for reporting syntax errors separately for user and test code
-    var userCodNumLines = userCod.split('\n').length;
-
-    var lang = $('#pythonVersionSelector').val();
-    if (lang === 'ts' || lang === 'js' || lang === 'java' || lang === 'c' || lang === 'cpp') {
-      var bufferCod = '\n\n// Test code //\n';
-    } else {
-      var bufferCod = '\n\n## Test code ##\n';
-    }
-
-    var bufferCodNumLines = bufferCod.split('\n').length;
-
-    var combinedCod = userCod + bufferCod + testCod;
-    return {cod: combinedCod,
-            firstTestLine: userCodNumLines + bufferCodNumLines - 1};
-  }
-
-  appStateAugmenter(appState) {
-    // returns a list of strings, each of which is a test case
-    function getAllTestcases() {
-      return $.map($("#testCasesParent #testCasesTable .testCaseEditor"),
-      (e) => {
-        var editor = ace.edit($(e).attr('id'));
-        return editor.getValue();
-      });
-    }
-
-    var tc = getAllTestcases();
-    if (tc.length > 0) {
-      appState['testCasesJSON'] = JSON.stringify(tc);
-    }
-  }
-
-} // END class OptTestcases
 
 
 var JS_EXAMPLES = {
@@ -1345,8 +1133,8 @@ $(document).ready(function() {
     assert(mod == 'display' || mod == 'visualize' /* deprecated */);
     var myArgs = optFrontend.getAppState();
     delete myArgs.mode;
-    myArgs.codeDivWidth = optFrontend.myVisualizer.DEFAULT_EMBEDDED_CODE_DIV_WIDTH;
-    myArgs.codeDivHeight = optFrontend.myVisualizer.DEFAULT_EMBEDDED_CODE_DIV_HEIGHT;
+    (myArgs as any).codeDivWidth = optFrontend.myVisualizer.DEFAULT_EMBEDDED_CODE_DIV_WIDTH;
+    (myArgs as any).codeDivHeight = optFrontend.myVisualizer.DEFAULT_EMBEDDED_CODE_DIV_HEIGHT;
 
     var domain = "http://pythontutor.com/"; // for deployment
     var embedUrlStr = $.param.fragment(domain + "iframe-embed.html", myArgs, 2 /* clobber all */);
