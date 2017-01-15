5d95d6b27147d90a92fa3968476bb8654a02561d
Philip Guo <philip@pgbovine.net>
2017-01-15 10:20:06 -0800

    yay progress

--- v5-unity/doctest_exec.py
+++ v5-unity/doctest_exec.py
@@ -15,12 +15,15 @@ Output:
 
 '''
 TODOs
-- get it running *with* security checks
 - return success/failure/errors/etc.
 
-- when students are trying to do printf-debugging, they will *always*
-  get test failures since doctests are based on prints, not actual return
-  values. hmmm, how do we fix this?!?
+- because doctests are based on stdout and not semantic value checking ...
+    - there is an ambiguity between 'return-ing' stuff and printing stuff to
+      the terminal. right now if students try to print, it will return None
+      and print an extra None, which could be confusing
+    - when students are trying to do printf-debugging inside of their
+      functions, they will *always* get test failures since doctests are based
+      on prints, not actual return values. hmmm, how do we fix this?!?
 '''
 
 import cPickle
@@ -46,13 +49,6 @@ def opt_run_doctest(doctest, example_number, student_code):
         #print example_to_run.source
 
         #exec compile(example_to_run.source, module_name, "single") in student_globals
-
-        # doesn't work :/ -- maybe resource restrictions because
-        # we're trying to use os functions in this file all over the place?
-        #pg_logger.exec_script_str(e.source, None, None, my_finalizer,
-        #                          custom_globals=globs)
-
-        # works
         opt_doctest_exec_script_str(example_to_run.source,
                                     my_finalizer,
                                     custom_modules={module_name: student_code})
@@ -70,12 +66,9 @@ def my_finalizer(input_code, output_trace):
   pp = pprint.PrettyPrinter()
   pp.pprint(output_trace)
 
-# disables security check and returns the result of finalizer_func
-# WARNING: ONLY RUN THIS LOCALLY and never over the web, since
-# security checks are disabled
 def opt_doctest_exec_script_str(script_str, finalizer_func, custom_modules):
   logger = pg_logger.PGLogger(False, False, False, finalizer_func,
-                              disable_security_checks=True,
+                              disable_security_checks=False,
                               custom_modules=custom_modules)
   try:
     logger._runscript(script_str)
@@ -102,53 +95,3 @@ if __name__ == "__main__":
                 opt_run_doctest(t, example_number, student_code)
                 break
 
-sys.exit(0)
-
-
-if __name__ == "__main__":
-    filename = sys.argv[1]
-    assert filename.endswith(".py")
-
-    # lifted from doctest.py
-    # It is a module -- insert its dir into sys.path and try to
-    # import it. If it is part of a package, that possibly
-    # won't work because of package imports.
-    dirname, filename = os.path.split(filename)
-    sys.path.insert(0, dirname)
-    m = __import__(filename[:-3]) # TODO: what about importing from a string -- exec'ing?
-    del sys.path[0]
-    dtf = doctest.DocTestFinder()
-    tests = dtf.find(m)
-    for t in tests:
-        globs = t.globs
-        #t.name
-        #t.filename
-        #t.docstring
-        #r = doctest.DocTestRunner()
-        #r.run(t, clear_globs=False)
-        #r.summarize(verbose=True)
-        for e in t.examples:
-            print e.source
-            # the 'single' mode produces the results to stdout, i think! ahhhhh
-            '''
-            https://docs.python.org/2/library/functions.html#compile
-
-            The mode argument specifies what kind of code must be
-            compiled; it can be 'exec' if source consists of a sequence
-            of statements, 'eval' if it consists of a single expression,
-            or 'single' if it consists of a single interactive statement
-            (in the latter case, expression statements that evaluate to
-            something other than None will be printed).
-            '''
-            #exec compile(e.source, filename, "single") in globs # works!
-
-            # doesn't work :/ -- maybe resource restrictions because
-            # we're trying to use os functions in this file all over the place?
-            #pg_logger.exec_script_str(e.source, None, None, my_finalizer,
-            #                          custom_globals=globs)
-
-            # works!
-            MY_exec_script_str_local(e.source, my_finalizer, custom_globals=globs)
-
-            #pg_logger.exec_script_str(e.source, None, None, my_finalizer)
-        print
