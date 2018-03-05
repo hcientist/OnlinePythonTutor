d1c96146b6bfee0bd836b43ca1144a66af83f5bf
Philip Guo <philip@pgbovine.net>
2018-03-05 04:29:31 +0000

    minor

--- v4-cokapi/linode-forever-processes.sh
+++ v4-cokapi/linode-forever-processes.sh
@@ -1,3 +1,4 @@
-cd ~/OnlinePythonTutor/v4-cokapi && jshint cokapi.js && forever -a -l cokapi.log start cokapi.js &
-cd ~/OnlinePythonTutor/v4-cokapi && jshint cokapi.js && forever -a -l cokapi-https.log start cokapi.js https &
-forever -a -l togetherjs.log --spinSleepTime 30000 -o togetherjs.out -e togetherjs.err start /home/pgbovine/OnlinePythonTutor/v3/opt_togetherjs/server.js --host 0.0.0.0 --port 30035 &
+#!/bin/sh
+echo "start `date`" >> /home/pgbovine/crontab-status.txt
+cd /home/pgbovine/OnlinePythonTutor/v4-cokapi && /home/pgbovine/node-v6.9.5-linux-x64/bin/forever -a -l /home/pgbovine/.forever/cokapi.log start cokapi.js > /home/pgbovine/cokapi.out 2> /home/pgbovine/cokapi.err
+cd /home/pgbovine/OnlinePythonTutor/v4-cokapi && /home/pgbovine/node-v6.9.5-linux-x64/bin/forever -a -l /home/pgbovine/.forever/cokapi-https.log start cokapi.js https > /home/pgbovine/cokapi-https.out 2> /home/pgbovine/cokapi-https.err
