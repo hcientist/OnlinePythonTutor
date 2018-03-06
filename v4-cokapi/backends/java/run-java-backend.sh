872aef2286155f7f4a1325c81582de2a9679a287
Philip Guo <philip@pgbovine.net>
2018-03-06 18:20:47 +0000

    added jdk1.8.0_20 into here so that it's all self-contained!

--- v4-cokapi/backends/java/run-java-backend.sh
+++ v4-cokapi/backends/java/run-java-backend.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
+# run this from within a Docker container
+
 # $1 is a string representing a JSON object that the Java backend
 # expects as input, such as: java_jail/cp/traceprinter/test-input.txt
-
-# this will be run within Docker
 
 # tricky! use a heredoc to pipe the $1 argument into the stdin of the
 # java executable WITHOUT interpreting escape chars such as '\n' ...
 # echo doesn't work here since it interprets '\n' and other chars
 #
 # TODO: try different -Xmx512m memory settings depending on user needs
-cat <<ENDEND | java -Xmx512m -cp /tmp/java_jail/cp:/tmp/java_jail/cp/javax.json-1.0.jar:/usr/lib/jvm/java-8-oracle/lib/tools.jar:/tmp/java_jail/cp/visualizer-stdlib traceprinter.InMemory
+cat <<ENDEND | /tmp/jdk1.8.0_20/bin/java -Xmx512m -cp /tmp/java_jail/cp:/tmp/java_jail/cp/javax.json-1.0.jar:/tmp/jdk1.8.0_20/lib/tools.jar:/tmp/java_jail/cp/visualizer-stdlib traceprinter.InMemory
 $1
 ENDEND
