de7863c9491d1682787cacca5821a08dd25dcc6e
Philip Guo <philip@pgbovine.net>
2018-03-07 06:48:44 +0000

    bah

--- v4-cokapi/backends/java/run-java-backend.sh
+++ v4-cokapi/backends/java/run-java-backend.sh
@@ -9,11 +9,11 @@
 # java executable WITHOUT interpreting escape chars such as '\n' ...
 # echo doesn't work here since it interprets '\n' and other chars
 
-# old memory value: -Xmx512m
+# old memory value: -Xmx512m (or was it -Xmx512M ?)
 #
-# OK, my current folk hypothesis is that the InMemory process should
+# OK, my current folk hypothesis is that this InMemory process should
 # *not* be given a lot of RAM, since that will simply eat up the limited
-# RAM quota in the docker container available to the child tracer process
+# RAM quota in the docker container available to the child process
 # that it launches!
 cat <<ENDEND | /tmp/jdk1.8.0_20/bin/java -cp /tmp/java_jail/cp:/tmp/java_jail/cp/javax.json-1.0.jar:/tmp/jdk1.8.0_20/lib/tools.jar:/tmp/java_jail/cp/visualizer-stdlib traceprinter.InMemory
 $1
