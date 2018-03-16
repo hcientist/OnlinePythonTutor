cb12deff5b2c3ee833fd1f0bd412d4db8a2f0917
Philip Guo <philip@pgbovine.net>
2018-03-15 23:09:08 -0700

    tiny

--- v5-unity/js/pytutor.ts
+++ v5-unity/js/pytutor.ts
@@ -3206,13 +3206,13 @@ class CodeDisplay {
     // the "Live Programming Mode" button in the code editor:
     //<span id="liveModeSpan" style="display: none;">| <a id="editLiveModeBtn" href="#">Live programming</a></a>\
     //
-    // also changed 'Edit code' link to 'Edit your code' to make
+    // also changed 'Edit code' link to 'Edit this code' to make
     // it more clear to users
     var codeDisplayHTML =
       '<div id="codeDisplayDiv">\
          <div id="langDisplayDiv"></div>\
          <div id="pyCodeOutputDiv"/>\
-         <div id="editCodeLinkDiv"><a id="editBtn">Edit your code</a>\
+         <div id="editCodeLinkDiv"><a id="editBtn">Edit this code</a>\
          </div>\
          <div id="legendDiv"/>\
          <div id="codeFooterDocs">Click a line of code to set a breakpoint; use the Back and Forward buttons to jump there.</div>\
