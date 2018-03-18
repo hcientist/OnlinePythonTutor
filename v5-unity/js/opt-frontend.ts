4ee3633c756bd5a21a204bf72c2364084b945ccb
Philip Guo <philip@pgbovine.net>
2018-03-18 10:40:58 -0700

    minor wording tweaks

--- v5-unity/js/opt-frontend.ts
+++ v5-unity/js/opt-frontend.ts
@@ -141,8 +141,9 @@ export class OptFrontend extends AbstractBaseFrontend {
     });
 
     $("#instructionsPane").html(`Advanced instructions:
-      <a href="https://www.youtube.com/watch?v=80ztTXP90Vs" target="_blank">setting breakpoints</a> |
-      <a href="https://www.youtube.com/watch?v=Mxt9HZWgwAM" target="_blank">hiding variables</a>`);
+      <a href="https://www.youtube.com/watch?v=80ztTXP90Vs&list=PLzV58Zm8FuBL2WxxZKGZ6j1dH8NKb_HYI&index=5" target="_blank">setting breakpoints</a> |
+      <a href="https://www.youtube.com/watch?v=Mxt9HZWgwAM&list=PLzV58Zm8FuBL2WxxZKGZ6j1dH8NKb_HYI&index=6" target="_blank">hiding variables</a> |
+      <a href="https://www.youtube.com/watch?v=JjGt95Te0wo&index=3&list=PLzV58Zm8FuBL2WxxZKGZ6j1dH8NKb_HYI" target="_blank">live programming</a>`);
 
     // first initialize options from HTML LocalStorage. very important
     // that this code runs FIRST so that options get overridden by query
