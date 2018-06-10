4b349498dc8da4ec0724bf560ee7e0de084fa0c4
Philip Guo <philip@pgbovine.net>
2018-06-10 11:02:16 -0700

    bam

--- v5-unity/tests/frontend-tests/run-all-tests.js
+++ v5-unity/tests/frontend-tests/run-all-tests.js
@@ -1,5 +1,15 @@
 const runFrontendTest = require('./frontend-golden-test').runFrontendTest;
 
 // TODO: we can separate this out later into different subsets
-runFrontendTest('py2', 'homepage.trace', 'a', {disableHeapNesting: true, startingInstruction: 15});
-runFrontendTest('py2', 'homepage.trace', 'b', {disableHeapNesting: false, startingInstruction: 15});
+
+// need to wrap in async to make it run serially
+(async () => {
+  await runFrontendTest('py2', 'homepage.trace', 'a', {disableHeapNesting: true, startingInstruction: 15});
+  await runFrontendTest('py2', 'homepage.trace', 'b', {disableHeapNesting: false, startingInstruction: 15});
+
+  await runFrontendTest('ruby', 'class-inheritance.trace', 'a', {disableHeapNesting: true, startingInstruction: 20});
+  await runFrontendTest('ruby', 'class-inheritance.trace', 'b', {disableHeapNesting: false, startingInstruction: 20});
+
+  await runFrontendTest('ruby', 'megagreeter.trace', 'a', {disableHeapNesting: true, startingInstruction: 40});
+  await runFrontendTest('ruby', 'megagreeter.trace', 'b', {disableHeapNesting: false, startingInstruction: 40});
+})();
