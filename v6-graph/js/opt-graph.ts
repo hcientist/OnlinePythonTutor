e2fac3a7cc98fa932d3838222626f5ea40397666
Stifler55 <diogo_xutos@hotmail.com>
2020-07-29 17:11:39 +0100

    add info about the graphs

--- v6-graph/js/opt-graph.ts
+++ v6-graph/js/opt-graph.ts
@@ -33,31 +33,69 @@ setReadMode(){
 }
 }
 
+
+
+
 $(document).ready(function() {
     optGraphFrontend = new OptGraphFrontend({});
+    var zoom = 1;
+    var zoomStep = 0.2;
 
-     $( "#graphSelector" ).change(function() {
-            var selectorVal = $('#graphSelector').val();
-            if (selectorVal === "FCG")
-                $("#functionSelector").hide();
-            else
-                $("#functionSelector").show();
+    var fcg = 'This graph represents calling relationships between functions. The blue nodes represent the functions defined by the user while the green ones the functions pre-defined.';
+    var cfg = "A control-flow graph (CFG) is a representation of all paths that might be traversed through a program during its execution.";
+    var dfg = "This graph represents data dependencies between variables. The green nodes represents a final state variable after an operation/value assignment. The red ones indicates this variable can have one of the previous states.";
+    document.getElementById('info').innerHTML = fcg;
+
+    $( "#graphSelector" ).change(function() {
+       var selectorVal = $('#graphSelector').val();
+       if (selectorVal === "FCG") {
+           $("#functionSelector").hide();
+           document.getElementById('info').innerHTML = fcg;
+       }
+       else if (selectorVal === "CFG") {
+           $("#functionSelector").show();
+           document.getElementById('info').innerHTML = cfg;
+       }
+       else {
+           $("#functionSelector").show();
+           document.getElementById('info').innerHTML = dfg;
+       }
      });
 
+    $("#imageid").click(function(){
+        document.getElementById('modal').style.display='block';
+    });
+
+    $("#close").click(function(){
+        zoom = 1
+        document.getElementById("modalimg").style.transform = "scale("+zoom+")";
+        document.getElementById('modal').style.display='none';
+    });
+
+    $("#zoom-in").click(function(){
+      zoom += zoomStep;
+      document.getElementById("modalimg").style.transform = "scale("+zoom+")";
+     });
+
+    $("#zoom-out").click(function(){
+        if(zoom > zoomStep){
+            zoom -= zoomStep;
+            document.getElementById("modalimg").style.transform = "scale("+zoom+")";
+        }
+    });
 
     $("#graphBtn").click(function() {
         var selectorVal = $('#graphSelector').val();
-        var funct :string = ""
+        var funct :string = "";
         if (selectorVal != "FCG")
             funct = $("#functionSelector").val();
-        var path = ""
+        var path = "";
         $.get("/viz_graph.py", {code: code, graph: selectorVal, func: funct} , function(data){
-            path = data
+            path = data;
             document.getElementById("imageid").setAttribute('src',data);
             document.getElementById("modalimg").setAttribute('src',data);
             $.get("/del_img.py", {path: path});
-        })
-
+        });
     });
 
 });
