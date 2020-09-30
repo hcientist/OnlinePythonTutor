cbeab33724b2dbc08986ca25cd5b8f114d9891fc
DaniMerchOut <70753263+DaniMerchOut@users.noreply.github.com>
2020-09-30 19:01:38 +0500

    Update map.py

--- tutorials/advanced/map.py
+++ tutorials/advanced/map.py
@@ -1,3 +1,6 @@
+def square_root(z):
+    return z^0.5
+
 def square(x):
     return x*x
 
@@ -8,4 +11,6 @@ def map(f, lst):
     return ret
 
 y = map(square, [1,2,3,4,5,6])
+
+a = map(square_root, [1,4,9,16,25,36])
 
